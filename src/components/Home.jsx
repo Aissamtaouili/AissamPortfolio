371632a28ab62dcb670d73ca497cc35153a35226
Aissam Taouili <abdlghani041@gmail.com>
2025-06-23 00:10:54 +0200

    last ver 23 m

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "../components/Banner.jsx";
 import About from "./About.jsx";
+import SkillsGrid from "./SkillsGrid.jsx";
 import Technologies from "./Technologies.jsx";
 import ParticleAnimation from "./ParticleAnimation.jsx";
 
@@ -13,6 +14,7 @@ function Home() {
       <Banner />
       <About />
       <Technologies />
+      <SkillsGrid />
     </div>
   );
 }
